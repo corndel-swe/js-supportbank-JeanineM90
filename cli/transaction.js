b865ca8c04139cfad7657a58c99fcbe1936e433f
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-07-01 15:18:39 +0100

    d1e3

--- cli/transaction.js
+++ cli/transaction.js
@@ -6,7 +6,7 @@ transactionController
   .command('log <from> <to> <amount>')
   .description('Log transaction data to the console')
   .action((from, to, amount) => {
-    // TODO
+    console.log(`logging transaction of ${amount} from ${from} to ${to}`)
   })
 
 export default transactionController
